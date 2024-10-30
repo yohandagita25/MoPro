2406b91680ff99c4c4bae23759d9cae599e41806
yohandagita25 <145240588+yohandagita25@users.noreply.github.com>
2024-10-30 12:09:23 +0700

    Add files via upload

---
+++ database1.php
@@ -0,0 +1,56 @@
+[
+    {
+        "id": "1",
+        "nama": "Michael White",
+        "jabatan": "Web Master",
+        "email": "michaelwhite@gmail.com",
+        "phone": "081321211344",
+        "gaji": "1.000.000",
+        "foto": "https://upload.wikimedia.org/wikipedia/commons/6/6c/Michael_White_photo.jpg"
+    },
+    {
+        "id": "2",
+        "nama": "Leonardo Dicaprio",
+        "jabatan": "Web Developher",
+        "email": "leodicaprio@gmail.com",
+        "phone": "081321211345",
+        "gaji": "2.000.000",
+        "foto": "https://upload.wikimedia.org/wikipedia/commons/4/46/Leonardo_Dicaprio_Cannes_2019.jpg"
+    },
+    {
+        "id": "3",
+        "nama": "Taylor Swift",
+        "jabatan": "Web Designer",
+        "email": "taylorswift@gmail.com",
+        "phone": "081321211346",
+        "gaji": "3.000.000",
+        "foto": "https://upload.wikimedia.org/wikipedia/commons/b/b1/Taylor_Swift_at_the_2023_MTV_Video_Music_Awards_%283%29.png"
+    },
+    {
+        "id": "4",
+        "nama": "Tom Cruise",
+        "jabatan": "Web Programmer",
+        "email": "tomcruise@gmail.com",
+        "phone": "081321211347",
+        "gaji": "4.000.000",
+        "foto": "https://upload.wikimedia.org/wikipedia/commons/3/33/Tom_Cruise_by_Gage_Skidmore_2.jpg"
+    },
+    {
+        "id": "5",
+        "nama": "Roger S.Pressman",
+        "jabatan": "System Analyst",
+        "email": "roger.spressman@gmail.com",
+        "phone": "081321211348",
+        "gaji": "5.000.000",
+        "foto": "https://m.media-amazon.com/images/I/9184FsZ6PDL._SX1488_CR0%2C0%2C1488%2C1488_.jpg"
+    },
+    {
+        "id": "6",
+        "nama": "Yohanda Tata",
+        "jabatan": "CEO",
+        "email": "yohandatata@gmail.com",
+        "phone": "081123456789",
+        "gaji": "20 .000.000",
+        "foto": "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Bae_Suzy_at_Premier_for_Suzy%3F_Suzy%3F%2C_22._Nov._2015_21.jpg/640px-Bae_Suzy_at_Premier_for_Suzy%3F_Suzy%3F%2C_22._Nov._2015_21.jpg"
+    }
+]

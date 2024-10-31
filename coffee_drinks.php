cf3615da5c36c6dfcf2f1b8f2a658f0ae4317acd
yohandagita25 <145240588+yohandagita25@users.noreply.github.com>
2024-10-31 09:26:51 +0700

    Add files via upload

---
+++ coffee_drinks.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `database-coffee-shop`
+ */
+
+/* `database-coffee-shop`.`coffee_drinks` */
+$coffee_drinks = array(
+  array('id' => '1','title' => 'Caffè Latte','details' => 'Rich, full-bodied espresso in steamed milk, lightly topped with foam. A caffè latte is simply a shot or two of bold, tasty espresso with fresh, sweet steamed milk over it.'),
+  array('id' => '2','title' => 'Cappuccino','details' => 'Espresso with steamed milk, topped with a deep layer of foam.'),
+  array('id' => '3','title' => 'Caffè Mocha','details' => 'Espresso with bittersweet mocha sauce and steamed milk, topped with sweetened whipped cream. Delightful.'),
+  array('id' => '4','title' => 'Caramel Macchiato','details' => 'Espresso combined with vanilla-flavoured syrup, milk and caramel sauce over ice. A Starbucks classic, chilled for a classic summer\'s day. To our signature espresso we add a creamy mix of vanilla syrup and cold milk poured over ice; it\'s then topped with our proprietary buttery caramel sauce. Sweet!'),
+  array('id' => '5','title' => 'Vanilla Sweet Cream Cold Brew','details' => 'Just before serving, our slow-steeped Cold Brew is topped with a delicate float of house-made vanilla sweet cream that cascades throughout the cup.'),
+  array('id' => '6','title' => 'Cold Brew','details' => 'Slow-steeped, small-batch and super smooth. To create our signature recipe, our team spent months experimenting with different brew times and coffee varietals. We specifically developed the Starbucks® Cold Brew Blend to heighten the rich, naturally sweet flavor created during the cold brewing process.'),
+  array('id' => '7','title' => 'Mango Passion Frappuccino','details' => 'A tropical mango and passionfruit infusion, perfectly blended with a fruity hibiscus tea and ice.'),
+  array('id' => '8','title' => 'Raspberry Currant Frappuccino','details' => 'Made with freshly brewed Teavana® tea, blended juice and ice.'),
+  array('id' => '9','title' => 'Coffee Frappuccino','details' => 'Starbucks® coffee is blended with nonfat milk and ice for a sip on the light side.'),
+  array('id' => '10','title' => 'Mocha Frappuccino','details' => 'Mocha sauce, Frappuccino® roast coffee, milk and ice all come together for a mocha flavor that\'ll leave you wanting more.')
+);
